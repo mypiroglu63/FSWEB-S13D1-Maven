0e9bb78657f5ca43333b1dc95fa996af80808497
mypiroglu63 <mypiroglu63@gmail.com>
2024-07-22 16:12:23 +0300

    Fonksiyonları Yazdım.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,51 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
-    }
+        if(isBarking && ((clock >= 20 && clock <= 23) || (clock >= 0 && clock < 8))) {
+            return true;
+        }
+            else {
+                return false;
+            }
+        }
+
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       if((firstAge >= 13 && firstAge <= 19) || (secondAge >= 13 && secondAge <= 19) || (thirdAge >= 13 && thirdAge <= 19)){
+           return true;
+       }
+       else {
+           return  false;
+       }
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+     if(isSummer && (temp >= 25 && temp <= 45)){
+         return true;
+     }
+     else if (!isSummer && (temp >= 25 && temp <= 35)){
+         return true;
+     }
+     else {
+         return false;
+     }
     }
 
     public static double area(double width, double height) {
-       
+       if((width < 0) || (height < 0)) {
+           return -1;
+        }
+       else {
+           return width * height;
+       }
     }
 
     public static double area(double radius) {
-       
+       if(radius < 0) {
+           return -1;
+       }
+       else {
+           return radius*radius*Math.PI;
+       }
     }
 }
